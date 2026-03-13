d13ff5ac53c906a70d8da70f8d9993f4ef04bb25
hoareausylvie <hoareauylvie@outlook.fr>
2026-03-13 16:49:13 +0400

    feat: ajout d'un formulaire pour ajouter un nouvel utilisateur

--- src/Controller/EcoTrackerController.php
+++ src/Controller/EcoTrackerController.php
@@ -4,13 +4,38 @@ namespace App\Controller;
 
 use App\Document\User;
 use App\Document\EcoAction;
+use App\Form\UserType;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EcoTrackerController extends AbstractController
 {
+    #[Route('/user/new', name: 'user_new')]
+    public function new(Request $request, DocumentManager $dm): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Le formulaire a rempli l'objet $user automatiquement
+            $dm->persist($user);
+            $dm->flush();
+
+            $this->addFlash('success', 'Utilisateur créé avec succès !');
+
+            return $this->redirectToRoute('eco_list');
+        }
+
+        return $this->render('user/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    } 
+
     #[Route('/eco/add', name: 'eco_add')]
     public function add(DocumentManager $dm): Response
     {
---
+++ src/Form/UserType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Document\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'label' => 'Nom d\'utilisateur',
+                'attr' => ['placeholder' => 'Ex: Vivien']
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse Email'
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Créer le compte',
+                'attr' => ['class' => 'btn btn-primary']
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
